d85461faa99b3463c64cd404ac1691d16c7deced
hoangquoctuan104-svg <hoangquoctuan104@gmail.com>
2026-01-19 17:21:53 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -21,3 +21,10 @@ export default defineConfig(({ mode }) => {
       }
     };
 });
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  base: './', // 确保加上这一行，这会让资源使用相对路径
+})
